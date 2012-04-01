3a2034ac23d9a5b38945b9a0e2d2d23bc54d23c1
HasegawaTadamitsu <hasegawa.tadamitsu@gmail.com>
2012-04-01 17:42:13 +0900

    indent check

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -42,47 +42,44 @@
 
 
 (when (eq window-system 'ns)
-    ;; asciiフォントをMenloに
-    (set-face-attribute 'default nil
-                                              :family "Menlo"
-                                                                    :height 120)
-      ;; 日本語フォントをヒラギノ明朝 Proに
-      (set-fontset-font
-          nil 'japanese-jisx0208
-             ;; 英語名の場合
-             ;; (font-spec :family "Hiragino Mincho Pro"))
-             (font-spec :family "ヒラギノ明朝 Pro"))
-        ;; ひらがなとカタカナをモトヤシーダに
+  ;; asciiフォントをMenloに
+  (set-face-attribute 'default nil
+                      :family "Menlo"
+                      :height 120)
+  ;; 日本語フォントをヒラギノ明朝 Proに
+  (set-fontset-font
+       nil 'japanese-jisx0208
+       ;; 英語名の場合
+       ;; (font-spec :family "Hiragino Mincho Pro"))
+       (font-spec :family "ヒラギノ明朝 Pro"))
+      ;; ひらがなとカタカナをモトヤシーダに
       ;; U+3000-303FCJKの記号および句読点
       ;; U+3040-309Fひらがな
       ;; U+30A0-30FFカタカナ
         (set-fontset-font
             nil '(#x3040 . #x30ff)
-               (font-spec :family "NfMotoyaCedar"))
-          ;; フォントの横幅を調節する
-          (setq face-font-rescale-alist
-                        '((".*Menlo.*" . 1.0)
-                                    (".*Hiragino_Mincho_Pro.*" . 1.2)
-                                              (".*nfmotoyacedar-bold.*" . 1.2)
-                                                        (".*nfmotoyacedar-medium.*" . 1.2)
-                                                                  ("-cdac$" . 1.3))))
+            (font-spec :family "NfMotoyaCedar"))
+      ;; フォントの横幅を調節する
+      (setq face-font-rescale-alist
+         '((".*Menlo.*" . 1.0)
+          (".*Hiragino_Mincho_Pro.*" . 1.2)
+          (".*nfmotoyacedar-bold.*" . 1.2)
+          (".*nfmotoyacedar-medium.*" . 1.2)
+          ("-cdac$" . 1.3))))
 
 (when (eq system-type 'windows-nt)
     ;; asciiフォントをConsolasに
     (set-face-attribute 'default nil
-                                              :family "Consolas"
-                                                                    :height 120)
+           :family "Consolas"
+           :height 120)
       ;; 日本語フォントをメイリオに
       (set-fontset-font
           nil
-             'japanese-jisx0208
-                (font-spec :family "メイリオ"))
-        ;; フォントの横幅を調節する
+         'japanese-jisx0208
+         (font-spec :family "メイリオ"))
+      ;; フォントの横幅を調節する
         (setq face-font-rescale-alist
-                      '((".*Consolas.*" . 1.0)
-                                  (".*メイリオ.*" . 1.15)
-                                            ("-cdac$" . 1.3))))
-
-
-
+            '((".*Consolas.*" . 1.0)
+             (".*メイリオ.*" . 1.15)
+             ("-cdac$" . 1.3))))
 
