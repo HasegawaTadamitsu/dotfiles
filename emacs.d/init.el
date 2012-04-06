364693f759728a93bde25a4b4085e5846ce8008e
HasegawaTadamitsu <hasegawa.tadamitsu@gmail.com>
2012-04-06 17:24:05 +0900

    hoge

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -113,7 +113,23 @@
         ;; 必要であればプロキシの設定を行う
         ;; (setq url-proxy-services '(("http" . "localhost:8339")))
         ;; 3●install-elisp の関数を利用可能にする
-        (auto-install-compatibility-setup))
+        (auto-install-compatibility-setup)
+        (setq auto-install-wget-command "hoge.sh") ;; ?
+)
+
+
+;; ▼要拡張機能インストール▼（ただし、Emacs24からはインストール不要）
+;;; P115-116 Emacs Lisp Package Archive（ELPA）──Emacs Lispパッケージマネージャ
+;; package.elの設定
+(when (require 'package nil t)
+
+  ;; パッケージリポジトリにMarmaladeと開発者運営のELPAを追加
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  ;; インストールしたパッケージにロードパスを通して読み込む
+  (package-initialize))
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -138,8 +154,8 @@
   (when (require 'anything-config nil t)
     ;; root権限でアクションを実行するときのコマンド
     ;; デフォルトは"su"
-    (setq anything-su-or-sudo "sudo"))
-
+    ;; (setq anything-su-or-sudo "sudo")
+  )
   (require 'anything-match-plugin nil t)
 
   (when (and (executable-find "cmigemo")
@@ -157,4 +173,101 @@
 
   (when (require 'descbinds-anything nil t)
     ;; describe-bindingsをAnythingに置き換える
-    (descbinds-anything-install)))
+    (descbinds-anything-install))
+
+    (define-key global-map (kbd "C-x b") 'anything)
+    (define-key global-map (kbd "M-y") 'anything-show-kill-ring)
+
+)
+
+(when (require 'anything-c-moccur nil t)
+  (setq
+   ;; anything-c-moccur用 `anything-idle-delay'
+   anything-c-moccur-anything-idle-delay 0.1
+   ;; バッファの情報をハイライトする
+   anything-c-moccur-higligt-info-line-flag t
+   ;; 現在選択中の候補の位置をほかのwindowに表示する
+   anything-c-moccur-enable-auto-look-flag t
+   ;; 起動時にポイントの位置の単語を初期パターンにする
+   anything-c-moccur-enable-initial-pattern t)
+  ;; C-M-oにanything-c-moccur-occur-by-moccurを割り当てる
+  (global-set-key (kbd "C-M-o") 'anything-c-moccur-occur-by-moccur)
+)
+
+
+;; ▼要拡張機能インストール▼
+;;; P133-134 moccurの結果を直接編集──moccur-edit
+;; moccur-editの設定
+(require 'moccur-edit nil t)
+;; moccur-edit-finish-editと同時にファイルを保存する
+;; (defadvice moccur-edit-change-file
+;;   (after save-after-moccur-edit-buffer activate)
+;;   (save-buffer))
+
+;; ▼要拡張機能インストール▼
+;;; P136 grepの結果を直接編集──wgrep
+;; wgrepの設定
+(require 'wgrep nil t)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.3 入力の効率化                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P130-131 利用可能にする
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories 
+    "~/.emacs.d/elisp/ac-dict")
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (ac-config-default))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 7.9 シェルの利用                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; ターミナルの利用 multi-term
+;; multi-termの設定
+(when (require 'multi-term nil t)
+  ;; 使用するシェルを指定
+  (setq multi-term-program "/usr/local/bin/zsh"))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.8 特殊な範囲の編集                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P151 矩形編集──cua-mode
+;; cua-modeの設定
+(cua-mode t) ; cua-modeをオン
+(setq cua-enable-cua-keys nil) ; CUAキーバインドを無効にする
+;; terminal の為、C-RETが使えない
+(define-key global-map (kbd "C-X SPC") 'cua-set-rectangle-mark)
+
+
+
+;; ▼要拡張機能インストール▼
+;;; P192-193 Anythingとタグの連携
+;; AnythingからTAGSを利用しやすくするコマンド作成
+(when (and (require 'anything-exuberant-ctags nil t)
+           (require 'anything-gtags nil t))
+  ;; anything-for-tags用のソースを定義
+  (setq anything-for-tags
+        (list anything-c-source-imenu
+              anything-c-source-gtags-select
+              ;; etagsを利用する場合はコメントを外す
+              ;; anything-c-source-etags-select
+              anything-c-source-exuberant-ctags-select
+              ))
+
+  ;; anything-for-tagsコマンドを作成
+  (defun anything-for-tags ()
+    "Preconfigured `anything' for anything-for-tags."
+    (interactive)
+    (anything anything-for-tags
+              (thing-at-point 'symbol)
+              nil nil nil "*anything for tags*"))
+  
+  ;; M-tにanything-for-currentを割り当て
+  (define-key global-map (kbd "M-t") 'anything-for-tags))
+
